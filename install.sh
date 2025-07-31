48b0c49b93e4bc8d6210544a08e4915224513f0d
fade_away <1028552010@qq.com>
2025-07-31 17:38:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,11 @@ mkdir -p /run/sshd
 
 user=$1
 passwd=$2
+
 # 添加用户，设置密码（不复用 root UID）
-useradd -m -s /bin/bash $user
-echo '$user:$passwd' | chpasswd
-usermod -aG sudo $user
+useradd -m -s /bin/bash "$user"
+echo "$user:$passwd" | chpasswd
+usermod -aG sudo "$user"
 
 # 修改 ssh 配置
 sed -i 's/^#\?\(PermitRootLogin\|PasswordAuthentication\).*/\1 yes/' /etc/ssh/sshd_config

355edd8063df4b6e8720ca86436dd5f72ab0341c
fade_away <1028552010@qq.com>
2025-07-31 17:32:05 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+apt update && apt install -y openssh-server sudo passwd nscd
+mkdir -p /run/sshd
+
+user=$1
+passwd=$2
+# 添加用户，设置密码（不复用 root UID）
+useradd -m -s /bin/bash $user
+echo '$user:$passwd' | chpasswd
+usermod -aG sudo alice
+
+# 修改 ssh 配置
+sed -i 's/^#\?\(PermitRootLogin\|PasswordAuthentication\).*/\1 yes/' /etc/ssh/sshd_config
